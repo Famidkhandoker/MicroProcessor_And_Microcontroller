485dc3b3fbdd32feb39a10d9cd2454b599790a66
FAMID KHANDOKER <famidkhandoker@gmail.com>
2026-06-10 12:02:12 +0600

    Add files via upload

---
+++ subtraction.asm
@@ -0,0 +1,55 @@
+.MODEL SMALL 
+.STACK 100H
+.DATA
+STR DW "Enter the first digit: $"
+STR2 DW "Enter the second digit: $"
+STR3 DW "Subtraction: $"
+.CODE
+MAIN PROC
+    
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,9
+    LEA DX,STR
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    MOV BH,AL
+    
+    MOV AH,2
+    MOV DL,10
+    INT 21H
+    MOV DL,13
+    INT 21H
+    
+    MOV AH,9
+    LEA DX,STR2
+    INT 21H    
+    
+    MOV AH,1
+    INT 21H
+    MOV CH,AL
+    
+    MOV AH,2
+    MOV DL,10
+    INT 21H
+    MOV DL,13
+    INT 21H
+    
+    MOV AH,9
+    LEA DX,STR3
+    INT 21H
+    
+    SUB BH,CH
+    
+    ADD BH,48
+    
+    MOV AH,2
+    MOV DL,BH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
+    
