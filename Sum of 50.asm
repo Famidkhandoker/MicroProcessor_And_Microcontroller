0419b60e089dc1fb0b702bfb5d7376d2efc3078a
FAMID KHANDOKER <famidkhandoker@gmail.com>
2026-06-17 11:50:03 +0600

    Add files via upload

---
+++ Sum of 50.asm
@@ -0,0 +1,22 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AX,0
+    MOV BX,50
+    MOV CX,1
+    
+    L:
+    ADD AX,CX
+    INC CX
+    
+    CMP CX,BX
+    JLE L
+    
+    EXIT:
+    MAIN ENDP
+END MAIN
